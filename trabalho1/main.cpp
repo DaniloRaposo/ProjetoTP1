43e20758c50f1cf976d6bfabd8625964a051e580
phalaggio <phalaggio@outlook.com>
2019-04-29 15:39:09 -0300

    toques finais
    dalta fazer o main rodar

--- trabalho1/main.cpp
+++ trabalho1/main.cpp
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+#include "dominios.h"
+#include "entidades.h"
+#include "testes.h"
+#include "testesdominios.h"
+
+    //este main roda todos os testes de dominio e entidade
+
+int main(){
+
+    TUCodigo_de_evento              testeCodigo_de_evento;
+    TUCodigo_de_apresentacao        testeCodigo_de_apresentacao;
+    TUCodigo_de_ingresso            testeCodigo_de_ingresso;
+    TUNome_de_evento                testeNome_de_evento;
+    TUData                          testeData;
+    TUHorario                       testeHorario;
+    TUPreco                         testePreco;
+    TUNumero_de_sala                testeNumero_de_sala;
+    TUCidade                        testeCidade;
+    TUEstado                        testeEstado;
+    TUDisponibilidade               testeDisponibilidade;
+    TUClasse_de_evento              testeClasse_de_evento;
+    TUFaixa_etaria                  testeFaixa_etaria;
+    TUCpf                           testeCpf;
+    TUSenha                         testeSenha;
+    TUNumero_de_cartao_de_credito   testeNumero_de_cartao_de_credito;
+    TUCodigo_de_seguranca           testeCodigo_de_seguranca;
+    TUData_de_validade              testeData_de_validade;
+
+
+
+
+}
